066257a8358397faa0ad6d526217a2b378ebd6f4
Thomas Leplus <thomasleplus@users.noreply.github.com>
2017-11-03 20:05:29 -0700

    Outline renaming of pi user

--- setup/rename-pi-user.sh
+++ setup/rename-pi-user.sh
@@ -3,3 +3,28 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+# check if root
+
+# check we are on a Raspberry Pi
+cat /proc/cpuinfo
+
+execs=("/bin/echo")
+for exec in "${execs[@]}"; do
+    if [ ! -x "${exec}" ]; then
+	>&2 /bin/echo "${0}: ${exec} required"
+	exit 1;
+    fi
+done
+
+/bin/echo -n "Type the desired username: "
+read username
+
+# if username does not exist, create it
+
+# if username pi still exist, copy groups (except pi) to new username
+# otherwise use default group list
+
+# sudo
+
+# if pi user still exist, ask to delete (default no)
+# if yes, delete user, group, home folder
