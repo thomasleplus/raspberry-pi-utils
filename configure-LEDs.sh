f32a9b9e40bee2922b0423a08201a0eb2a944695
Thomas Leplus <thomasleplus@users.noreply.github.com>
2025-08-13 16:15:28 +0200

    style: remove unnecessary escape for builtin

--- configure-LEDs.sh
+++ configure-LEDs.sh
@@ -4,14 +4,14 @@ IFS=$'\n\t'
 
 # check if root
 if [ "${EUID}" -ne 0 ]; then
-   >&2 \echo "Please run as root"
+   >&2 echo "Please run as root"
    exit 1
 fi
 
 execs=("/bin/grep" "/bin/sed" "/usr/bin/tee")
 for exec in "${execs[@]+"${execs[@]}"}"; do
     if [ ! -x "${exec}" ]; then
-	>&2 \echo "${0}: ${exec} required"
+	>&2 echo "${0}: ${exec} required"
 	exit 1
     fi
 done
@@ -20,7 +20,7 @@ set_dtparam() {
     if \grep -q "^dtparam=${1}" /boot/config.txt ; then
 	\sed -i "s/^dtparam=${1}/dtparam=${2}/g" /boot/config.txt
     else
-	\echo "dtparam=${2}" | \tee -a /boot/config.txt > /dev/null
+	echo "dtparam=${2}" | \tee -a /boot/config.txt > /dev/null
     fi
 }
 
@@ -30,7 +30,7 @@ if \grep -q " Zero " /proc/device-tree/model ; then
 fi
 
 ACT=0
-\echo -n "Enable ACT LED? [y/N] "
+echo -n "Enable ACT LED? [y/N] "
 read -r REPLY
 if [[ "${REPLY}" =~ ^[Yy] ]]; then
     ACT=1
@@ -38,7 +38,7 @@ fi
 
 if [ "${ZERO}" -ne 1 ]; then
     PWR=0
-    \echo -n "Enable PWR LED? [y/N] "
+    echo -n "Enable PWR LED? [y/N] "
     read -r REPLY
     if [[ "${REPLY}" =~ ^[Yy] ]]; then
         PWR=1
@@ -49,30 +49,30 @@ if [ "${ZERO}" -eq 0 ]; then
     if [ "${ACT}" -eq 0 ]; then # ACT off
 	set_dtparam "act_led_trigger=[^ 	]*" "act_led_trigger=none"
 	set_dtparam "act_led_activelow=[^ 	]*" "act_led_activelow=off"
-	\echo "ACT disabled."
+	echo "ACT disabled."
     else # ACT on
 	set_dtparam "act_led_trigger=[^ 	]*" "act_led_trigger=mmc0"
 	set_dtparam "act_led_activelow=[^ 	]*" "act_led_activelow=on"
-	\echo "ACT enabled."
+	echo "ACT enabled."
     fi
     if [ "${PWR}" -eq 0 ]; then # PWR off
 	set_dtparam "pwr_led_trigger=[^ 	]*" "pwr_led_trigger=none"
 	set_dtparam "pwr_led_activelow=[^ 	]*" "pwr_led_activelow=off"
-	\echo "PWR disabled."
+	echo "PWR disabled."
     else # PWR on
 	set_dtparam "pwr_led_trigger=[^ 	]*" "pwr_led_trigger=input"
 	set_dtparam "pwr_led_activelow=[^ 	]*" "pwr_led_activelow=on"
-	\echo "PWR enabled."
+	echo "PWR enabled."
     fi
 else
     # For Raspberry Pi Zero values are reversed
     if [ "${ACT}" -eq 0 ]; then # ACT off
 	set_dtparam "act_led_trigger=[^ 	]*" "act_led_trigger=none"
 	set_dtparam "act_led_activelow=[^ 	]*" "act_led_activelow=on"
-	\echo "ACT disabled."
+	echo "ACT disabled."
     else # ACT on
 	set_dtparam "act_led_trigger=[^ 	]*" "act_led_trigger=mmc0"
 	set_dtparam "act_led_activelow=[^ 	]*" "act_led_activelow=off"
-	\echo "ACT enabled."
+	echo "ACT enabled."
     fi
 fi
